cdeca220ed48d7f5845984d6df05746f64f96894
anubissmile <anubis.smile@gmail.com>
2018-04-08 06:30:24 +0700

    Improvement - View data table git data grid view.

--- Employee/Form1.Designer.vb
+++ Employee/Form1.Designer.vb
@@ -22,9 +22,126 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TestdbDataSet = New Employee.testdbDataSet()
+        Me.TestdbDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ResuserBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.Res_userTableAdapter = New Employee.testdbDataSetTableAdapters.res_userTableAdapter()
+        Me.TestdbDataSetBindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LastnameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AgeDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BirthdateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CreateddateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TestdbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TestdbDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ResuserBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TestdbDataSetBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.LastnameDataGridViewTextBoxColumn, Me.AgeDataGridViewTextBoxColumn, Me.BirthdateDataGridViewTextBoxColumn, Me.CreateddateDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.ResuserBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(108, 36)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(766, 150)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'TestdbDataSet
+        '
+        Me.TestdbDataSet.DataSetName = "testdbDataSet"
+        Me.TestdbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'TestdbDataSetBindingSource
+        '
+        Me.TestdbDataSetBindingSource.DataSource = Me.TestdbDataSet
+        Me.TestdbDataSetBindingSource.Position = 0
+        '
+        'ResuserBindingSource
+        '
+        Me.ResuserBindingSource.DataMember = "res_user"
+        Me.ResuserBindingSource.DataSource = Me.TestdbDataSetBindingSource
+        '
+        'Res_userTableAdapter
+        '
+        Me.Res_userTableAdapter.ClearBeforeFill = True
+        '
+        'TestdbDataSetBindingSource1
+        '
+        Me.TestdbDataSetBindingSource1.DataSource = Me.TestdbDataSet
+        Me.TestdbDataSetBindingSource1.Position = 0
+        '
+        'IdDataGridViewTextBoxColumn
+        '
+        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "id"
+        Me.IdDataGridViewTextBoxColumn.HeaderText = "id"
+        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        Me.IdDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'LastnameDataGridViewTextBoxColumn
+        '
+        Me.LastnameDataGridViewTextBoxColumn.DataPropertyName = "lastname"
+        Me.LastnameDataGridViewTextBoxColumn.HeaderText = "lastname"
+        Me.LastnameDataGridViewTextBoxColumn.Name = "LastnameDataGridViewTextBoxColumn"
+        '
+        'AgeDataGridViewTextBoxColumn
+        '
+        Me.AgeDataGridViewTextBoxColumn.DataPropertyName = "age"
+        Me.AgeDataGridViewTextBoxColumn.HeaderText = "age"
+        Me.AgeDataGridViewTextBoxColumn.Name = "AgeDataGridViewTextBoxColumn"
+        '
+        'BirthdateDataGridViewTextBoxColumn
+        '
+        Me.BirthdateDataGridViewTextBoxColumn.DataPropertyName = "birthdate"
+        Me.BirthdateDataGridViewTextBoxColumn.HeaderText = "birthdate"
+        Me.BirthdateDataGridViewTextBoxColumn.Name = "BirthdateDataGridViewTextBoxColumn"
+        '
+        'CreateddateDataGridViewTextBoxColumn
+        '
+        Me.CreateddateDataGridViewTextBoxColumn.DataPropertyName = "created_date"
+        Me.CreateddateDataGridViewTextBoxColumn.HeaderText = "created_date"
+        Me.CreateddateDataGridViewTextBoxColumn.Name = "CreateddateDataGridViewTextBoxColumn"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(997, 366)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TestdbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TestdbDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ResuserBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TestdbDataSetBindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TestdbDataSetBindingSource As BindingSource
+    Friend WithEvents TestdbDataSet As testdbDataSet
+    Friend WithEvents ResuserBindingSource As BindingSource
+    Friend WithEvents Res_userTableAdapter As testdbDataSetTableAdapters.res_userTableAdapter
+    Friend WithEvents IdDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents LastnameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents AgeDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents BirthdateDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CreateddateDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TestdbDataSetBindingSource1 As BindingSource
 End Class
--- Employee/Form1.vb
+++ Employee/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub TestdbDataSetBindingSource_CurrentChanged(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'TestdbDataSet.res_user' table. You can move, or remove it, as needed.
+        Me.Res_userTableAdapter.Fill(Me.TestdbDataSet.res_user)
+        'TODO: This line of code loads data into the 'TestdbDataSet.res_user' table. You can move, or remove it, as needed.
+        Me.Res_userTableAdapter.Fill(Me.TestdbDataSet.res_user)
+
+    End Sub
 End Class
