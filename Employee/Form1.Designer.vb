4289bbf42d5a8de37f5e0166b67dc3e25e49ed49
anubissmile <anubis.smile@gmail.com>
2018-04-08 08:16:58 +0700

    Improvement - Select all and showing in data grid view.

--- Employee/Form1.Designer.vb
+++ Employee/Form1.Designer.vb
@@ -23,13 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.LastnameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.AgeDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.BirthdateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.CreateddateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.userView = New System.Windows.Forms.DataGridView()
         Me.ResuserBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.TestdbDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.TestdbDataSet = New Employee.testdbDataSet()
@@ -44,61 +38,22 @@ Partial Class Form1
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.btnRegister = New System.Windows.Forms.Button()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.btnShowAll = New System.Windows.Forms.Button()
+        CType(Me.userView, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.ResuserBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TestdbDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TestdbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.TestdbDataSetBindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'DataGridView1
+        'userView
         '
-        Me.DataGridView1.AutoGenerateColumns = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.LastnameDataGridViewTextBoxColumn, Me.AgeDataGridViewTextBoxColumn, Me.BirthdateDataGridViewTextBoxColumn, Me.CreateddateDataGridViewTextBoxColumn})
-        Me.DataGridView1.DataSource = Me.ResuserBindingSource
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 118)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(973, 236)
-        Me.DataGridView1.TabIndex = 0
-        '
-        'IdDataGridViewTextBoxColumn
-        '
-        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "id"
-        Me.IdDataGridViewTextBoxColumn.HeaderText = "id"
-        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
-        Me.IdDataGridViewTextBoxColumn.ReadOnly = True
-        '
-        'NameDataGridViewTextBoxColumn
-        '
-        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "name"
-        Me.NameDataGridViewTextBoxColumn.HeaderText = "name"
-        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
-        '
-        'LastnameDataGridViewTextBoxColumn
-        '
-        Me.LastnameDataGridViewTextBoxColumn.DataPropertyName = "lastname"
-        Me.LastnameDataGridViewTextBoxColumn.HeaderText = "lastname"
-        Me.LastnameDataGridViewTextBoxColumn.Name = "LastnameDataGridViewTextBoxColumn"
-        '
-        'AgeDataGridViewTextBoxColumn
-        '
-        Me.AgeDataGridViewTextBoxColumn.DataPropertyName = "age"
-        Me.AgeDataGridViewTextBoxColumn.HeaderText = "age"
-        Me.AgeDataGridViewTextBoxColumn.Name = "AgeDataGridViewTextBoxColumn"
-        '
-        'BirthdateDataGridViewTextBoxColumn
-        '
-        Me.BirthdateDataGridViewTextBoxColumn.DataPropertyName = "birthdate"
-        Me.BirthdateDataGridViewTextBoxColumn.HeaderText = "birthdate"
-        Me.BirthdateDataGridViewTextBoxColumn.Name = "BirthdateDataGridViewTextBoxColumn"
-        '
-        'CreateddateDataGridViewTextBoxColumn
-        '
-        Me.CreateddateDataGridViewTextBoxColumn.DataPropertyName = "created_date"
-        Me.CreateddateDataGridViewTextBoxColumn.HeaderText = "created_date"
-        Me.CreateddateDataGridViewTextBoxColumn.Name = "CreateddateDataGridViewTextBoxColumn"
+        Me.userView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.userView.Location = New System.Drawing.Point(21, 109)
+        Me.userView.Name = "userView"
+        Me.userView.RowTemplate.Height = 24
+        Me.userView.Size = New System.Drawing.Size(973, 236)
+        Me.userView.TabIndex = 0
         '
         'ResuserBindingSource
         '
@@ -195,18 +150,28 @@ Partial Class Form1
         '
         'btnRegister
         '
-        Me.btnRegister.Location = New System.Drawing.Point(765, 39)
+        Me.btnRegister.Location = New System.Drawing.Point(684, 44)
         Me.btnRegister.Name = "btnRegister"
         Me.btnRegister.Size = New System.Drawing.Size(139, 45)
         Me.btnRegister.TabIndex = 10
         Me.btnRegister.Text = "Register"
         Me.btnRegister.UseVisualStyleBackColor = True
         '
+        'btnShowAll
+        '
+        Me.btnShowAll.Location = New System.Drawing.Point(855, 44)
+        Me.btnShowAll.Name = "btnShowAll"
+        Me.btnShowAll.Size = New System.Drawing.Size(139, 45)
+        Me.btnShowAll.TabIndex = 11
+        Me.btnShowAll.Text = "Show All"
+        Me.btnShowAll.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(997, 366)
+        Me.ClientSize = New System.Drawing.Size(1009, 374)
+        Me.Controls.Add(Me.btnShowAll)
         Me.Controls.Add(Me.btnRegister)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -216,10 +181,10 @@ Partial Class Form1
         Me.Controls.Add(Me.txtLastname)
         Me.Controls.Add(Me.txtAge)
         Me.Controls.Add(Me.txtName)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.userView)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.userView, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.ResuserBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TestdbDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.TestdbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
@@ -229,17 +194,11 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents userView As DataGridView
     Friend WithEvents TestdbDataSetBindingSource As BindingSource
     Friend WithEvents TestdbDataSet As testdbDataSet
     Friend WithEvents ResuserBindingSource As BindingSource
     Friend WithEvents Res_userTableAdapter As testdbDataSetTableAdapters.res_userTableAdapter
-    Friend WithEvents IdDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents NameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents LastnameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents AgeDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents BirthdateDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-    Friend WithEvents CreateddateDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
     Friend WithEvents TestdbDataSetBindingSource1 As BindingSource
     Friend WithEvents txtName As TextBox
     Friend WithEvents txtAge As TextBox
@@ -250,4 +209,5 @@ Partial Class Form1
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents btnRegister As Button
+    Friend WithEvents btnShowAll As Button
 End Class
--- Employee/Form1.vb
+++ Employee/Form1.vb
@@ -10,21 +10,34 @@ Public Class Form1
         Me.Res_userTableAdapter.Fill(Me.TestdbDataSet.res_user)
         'TODO: This line of code loads data into the 'TestdbDataSet.res_user' table. You can move, or remove it, as needed.
         Me.Res_userTableAdapter.Fill(Me.TestdbDataSet.res_user)
-
+        ExecuteQuery("")
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnRegister.Click
 
         Dim insertExecute As String = "INSERT INTO dbo.res_user (name, lastname, age, birthdate, created_date, updated_date) VALUES ('" & txtName.Text & "', '" & txtLastname.Text & "', " & txtAge.Text & ", '" & txtBirth.Value & "', GETUTCDATE(), GETUTCDATE())"
-        ExecuteQuery(insertExecute)
-
-
+        ExecuteNonQuery(insertExecute)
+        ExecuteQuery("")
     End Sub
 
-    Public Sub ExecuteQuery(query As String)
+    Public Sub ExecuteNonQuery(query As String)
         Dim command As New SqlCommand(query, connection)
         connection.Open()
         command.ExecuteNonQuery()
         connection.Close()
+    End Sub
+
+    Private Sub btnShowAll_Click(sender As Object, e As EventArgs) Handles btnShowAll.Click
+        ExecuteQuery("")
+    End Sub
+
+    Public Sub ExecuteQuery(query As String)
+        Dim selectAll As String = "SELECT * FROM res_user"
+        Dim command As New SqlCommand(selectAll, connection)
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+
+        userView.DataSource = table
     End Sub
 End Class
